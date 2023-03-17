b68ff431671aff1b1c111355dc94ccb81ce8d0e7
kharerishabh <rishabkhare41@gmail.com>
2023-03-17 12:24:19 +0530

    using Link and children param

--- router-project/src/App.js
+++ router-project/src/App.js
@@ -1,20 +1,34 @@
-import {createBrowserRouter, RouterProvider, Route, createRoutesFromElements} from 'react-router-dom';
-import HomePage from './pages/Home';
-import ProductPage from './pages/Products';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/Products";
+import Root from "./pages/Root";
+import Error from "./pages/Error";
 
-const routeDefinations = createRoutesFromElements(
-  <Route>
-    <Route path='/' element={<HomePage/>} />
-    <Route path='/product' element={<ProductPage/>}/>
-  </Route>
-)
-const router = createBrowserRouter(routeDefinations)
-// // const router = createBrowserRouter([
-// //   {path: '/', element: <HomePage/>},
-// //   {path: '/product', element: <ProductPage/>}
-// // ])
+// const routeDefinations = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage/>} />
+//     <Route path='/product' element={<ProductPage/>}/>
+//   </Route>
+// )
+// const router = createBrowserRouter(routeDefinations)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <Error/>,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/product", element: <ProductPage /> },
+    ],
+  },
+]);
 function App() {
-  return (<RouterProvider router={router}/>);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- router-project/src/pages/Home.js
+++ router-project/src/pages/Home.js
@@ -1,4 +1,8 @@
+import { Link } from "react-router-dom";
 const HomePage = () => {
-    return (<h1>My Home Page</h1>)
+    return (<>
+    <h1>My Home Page</h1>
+    <p>Go to <Link to="/product"> the list of product</Link></p>
+        </>)
 }
 export default HomePage;
