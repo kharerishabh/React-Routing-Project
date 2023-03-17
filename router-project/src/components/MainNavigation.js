a0c36d633f4bb6281bddca4a989260318a504a85
kharerishabh <rishabkhare41@gmail.com>
2023-03-17 12:39:17 +0530

    using Navlink

--- router-project/src/components/MainNavigation.js
+++ router-project/src/components/MainNavigation.js
@@ -1,19 +1,35 @@
-import { Link } from "react-router-dom"
-import classes from './MainNavigation.module.css'
+import { NavLink } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 const MainNavigation = () => {
-    return (
-        <header className={classes.header}>
-            <nav>
-                <ul className={classes.list}>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/product'>Products</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-export default MainNavigation
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              style={({isActive}) => ({textAlign: isActive ? 'center' : 'left'})}
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/product"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default MainNavigation;
